8e3d58b27bca4c5fd290e33dd9c02a566aa9c51d
Wazabi <arthur.daniel@ynov.com>
2018-07-03 14:41:56 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Header from './Component/Header/Header';
 import Grid from './Component/Body/Grid';
 import { hot } from 'react-hot-loader';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Router>
         <Header/>
         <Grid/>
-        
+        <Route exact path="/" component={App} />
+      </Router>  
       </div>
     );
   }
