f9e1d51631b11bdc41d6caa4866aadbb417b2ce0
Wazabi <arthur.daniel@ynov.com>
2018-07-03 15:34:00 +0200

    bodyannonce

---
+++ src/Component/Annonce/BodyAnnonce.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class BodyAnnonce extends Component{
+    render(){
+        return(
+            <div>
+            </div>
+        );
+    }
+}
+
+export default BodyAnnonce;
