4bdabbc769e1d5411b1d3c425d2a3920c58fb23d
Wazabi <arthur.daniel@ynov.com>
2018-07-04 17:39:01 +0200

    chhh

--- server/App.js
+++ server/App.js
@@ -1,27 +1,60 @@
 const express = require('express');
-const session = require('cookie-session'); // Charge le middleware de sessions
-const bodyParser = require('body-parser');
-// Charge le middleware de gestion des paramètres
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const app = express();
-const mysql = require('mysql');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const task = require('./api/task');
+var hash = require('object-hash');
+var nodemailer = require('nodemailer');
 
 
+app.use(bodyParser.json({ extended: false }));
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'rentcar',
+// Route principale
+app.get('/', (req, res) => {
+	task.tempDataExist();
+	res.redirect('/patients');
 });
 
+// Route pour afficher tous les patients
+app.get('/patients', (req, res) => {
+ 	res.sendFile(path.join(`${__dirname}/test.json`));
+});
 
-/* On utilise les sessions */
-app.use(session({ secret: 'test' }))
+// Route pour sélectionner un patient spécifique et afficher les listes des patients sélectionnés
+app.get('/patient/:id', (req, res) => {
+	task.displayPatient(req.params.id);
+	res.sendFile(path.join(`${__dirname}/dataPatientDisplay.json`));
+});
 
-.post('/', urlencodedParser, (req, res) => {
+// Route pour envoyer par email la sélèction des patients
+app.post('/patient/mail', (req, res) => {
+	task.mailPatients();
+	res.redirect('/patients');
+});
 
-})
+// Route pour ajouter un patient
+app.post('/patient', (req, res) => {
+	var obj = req.body;
+	task.addOnePatient(obj);
+ 	res.redirect('/patients');
+});
 
-  .listen(8080);
+// Route pour supprimer un patient
+app.delete('/patient/delete/:id', (req,res) => {
+	task.deleteP(req.params.id);
+	res.redirect('/patients');
+});
+
+// Route pour modifier un patient
+app.put('/patient/:id', (req, res) => {
+	var id = req.params.id;
+  	var obj = req.body;
+  	task.modifyPatient(id, obj);
+  	res.redirect('/patients');
+});
+
+app.listen(1345, () => {
+ 	console.log('Server running on port 1345...');
+});
